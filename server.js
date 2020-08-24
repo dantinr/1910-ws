46649944b8c580a44e02ee688be0fe2b440ff448
dante <dantinr@gmail.com>
2020-08-24 18:21:06 +0800

    +moment package

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const WebSocket = require('ws');
+var moment = require('moment');
  
 const wss = new WebSocket.Server({ port: 8080 });
  
 wss.on('connection', function connection(ws) {
 	//接收客户端数据
 	ws.on('message', function incoming(message) {
-
-		ws.send(message);	//向客户端发送数据
+		var t = moment().format();		//当前时间
+		console.log(t);
+		ws.send(message + " " + t);		//向客户端发送数据
 
 	});
 
