99cffc7b2d360738da3beb5cf1f7a2ff8517990c
dante <dantinr@gmail.com>
2020-08-25 09:52:30 +0800

    聊天数据入库

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 const WebSocket = require('ws');
-var moment = require('moment');
- 
-const wss = new WebSocket.Server({ port: 8080 });
- 
-wss.on('connection', function connection(ws) {
-	//接收客户端数据
-	ws.on('message', function incoming(message) {
-		var t = moment().format();		//当前时间
-		console.log(t);
-		ws.send(message + " " + t);		//向客户端发送数据
+const moment = require('moment');
+const MongoClient = require('mongodb').MongoClient
 
-		//广播 向所有在线用户发送数据
-		wss.clients.forEach(function each(client) {
-			if ( client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(message + " " + t);
-			}
+
+
+//连接MongoDB数据库
+const mongoUrl = 'mongodb://localhost:27017/shop1910'
+//连接选项
+const mongoOption = {
+	useUnifiedTopology: true,
+	auth:{user:"trump",password:"123456abc"}
+}
+const wss = new WebSocket.Server({ port: 8080 });
+
+MongoClient.connect(mongoUrl,mongoOption,function(err,client){
+
+	const db = client.db();
+	const collection = db.collection('chat_msg')	//选择集合（表）
+
+	wss.on('connection', function connection(ws) {
+		//接收客户端数据
+		ws.on('message', function incoming(message) {
+			var t = moment().format();		//当前时间
+			ws.send(message + " " + t);		//向客户端发送数据
+
+			//将数据保存至 mongodb
+			var doc = {msg:message,time:t};
+			collection.insertOne(doc,function(err,result){
+				console.log(err);
+				console.log(message + " 消息入库");
+			})
+			//广播 向所有在线用户发送数据
+			wss.clients.forEach(function each(client) {
+				if ( client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(message + " " + t);
+				}
+			});
 		});
 
 	});
 
 });
+
+
+
 
